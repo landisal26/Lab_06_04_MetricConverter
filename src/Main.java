75d5ad3aaced4a648034f1e311ed9c195e267c3f
landisal <landisal@mail.uc.edu>
2022-10-11 13:36:07 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+    Scanner in = new Scanner(System.in);
+        double meter = 0;
+        double inches = 0;
+
+
+        System.out.println("Enter your distance in Meters: ");
+
+        if (in.hasNextDouble())
+        {
+            meter = in.nextDouble();
+        }
+        else
+        {
+            System.out.println("Not a valid input");
+            System.exit(0);
+        }
+
+        inches = 39.37 * meter;
+        int miles = (int) (inches/63360);
+        int feet = (int) (inches-miles*63360)/12;
+        double inchesLeft = inches-((miles*63360)-(feet*12));
+
+        System.out.println("Meters converted equals: " +miles+" Miles " +feet+" Feet " +inches+" Inches 540");
+
+
+
     }
 }
